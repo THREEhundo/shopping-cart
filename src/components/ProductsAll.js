a7cbbc5550562132130829a49eece168b66cbf2c
threehundo <samuelbaik87@gmail.com>
2021-03-12 08:36:03 -0800

    Created function to convert sneaker name to router link.

--- src/components/ProductsAll.js
+++ src/components/ProductsAll.js
@@ -1,8 +1,26 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 
 const ProductsAll = (props) => {
   const { sneakerDB } = props;
 
+  function formatName(name) {
+    const slash = "/";
+    const lCase = name.toLowerCase();
+    const dash = lCase.replaceAll(" ", "-");
+    console.log(slash.concat(dash));
+    return slash.concat(dash);
+  }
+  if (sneakerDB !== null) {
+    formatName(sneakerDB[0].name);
+  }
   function cardStructure(props) {
     if (sneakerDB !== null) {
       return sneakerDB.map((snkr) => {
@@ -11,11 +29,13 @@ const ProductsAll = (props) => {
             key={snkr.id}
             className="flex flex-col self-center justify-evenly rounded-lg border-2 border-white w-11/12 h-auto py-2 px-2 my-2 mx-auto"
           >
+            {/* <Link to={formatName(snkr.name)}> */}
             <img
               className="rounded-t-xl"
               alt="snkrImg"
               src={snkr.img.smallImg}
             ></img>
+            {/* </Link> */}
             <h2 className="text-lg">{snkr.shoe}</h2>
             <h2 className="text-base">"{snkr.name}"</h2>
             <p className="text-xs">Released: {snkr.releaseDate}</p>

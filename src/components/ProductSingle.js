525ee135c0a65f3c9572cac751ee84bcc56efc8c
threehundo <samuelbaik87@gmail.com>
2021-03-22 11:13:09 -0700

    Added container div for description of product and full screen height for product div in ProductSingle component.

--- src/components/ProductSingle.js
+++ src/components/ProductSingle.js
@@ -27,29 +27,32 @@ const ProductSingle = ({
       const copy = data.map((x) => x);
       const snkr = copy.find((x) => x.linkID === linkID);
       mod = (
-        <div className="h-screen p-5" key={snkr.id} id={snkr.id}>
+        <div className="h-auto p-16" key={snkr.id} id={snkr.id}>
           <img
             className="rounded-t-xl"
             alt="snkrImg"
             src={snkr.img.smallImg}
           ></img>
-          <h2 className="text-3xl">{snkr.shoe}</h2>
-          <h2 className="text-3xl">"{snkr.name}"</h2>
-          <h2 className="text-3xl">${snkr.retailPrice}</h2>
-          <p className="text-xs">Released: {snkr.releaseDate}</p>
-          <p className="text-xs">
-            Since his game-winning shot that brought championship glory to North
-            Carolina, Michael Jordan has been at the forefront of basketball
-            consciousness. He took the court in 1985 wearing the original Air
-            Jordan I, simultaneously breaking league rules and his opponents'
-            will while capturing the imagination of fans worldwide.
-          </p>
-          <button
-            className="bg-secondary text-primary rounded-lg ring-4 ring-primary my-2 rounded-full py-1 px-2 hover:text-secondary hover:bg-primary hover:ring-secondary focus:outline-none focus:bg-primary focus:ring-secondary focus:text-secondary"
-            onClick={addToCart}
-          >
-            Buy
-          </button>
+          <div className="text-center">
+            <h2 className="text-3xl py-1">{snkr.shoe}</h2>
+            <h2 className="text-5xl py-1">"{snkr.name}"</h2>
+            <h2 className="text-3xl py-1">${snkr.retailPrice}</h2>
+            <p className="text-xs py-1">Released: {snkr.releaseDate}</p>
+            <p className="text-xs m-auto w-64 py-1">
+              Since his game-winning shot that brought championship glory to
+              North Carolina, Michael Jordan has been at the forefront of
+              basketball consciousness. He took the court in 1985 wearing the
+              original Air Jordan I, simultaneously breaking league rules and
+              his opponents' will while capturing the imagination of fans
+              worldwide.
+            </p>
+            <button
+              className="bg-secondary text-primary rounded-lg ring-4 ring-primary my-2 rounded-full py-1 px-2 hover:text-secondary hover:bg-primary hover:ring-secondary focus:outline-none focus:bg-primary focus:ring-secondary focus:text-secondary py-1"
+              onClick={addToCart}
+            >
+              Add to Cart
+            </button>
+          </div>
         </div>
       );
     }

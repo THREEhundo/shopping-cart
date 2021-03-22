7c1b0b8dd0508a5929ce395ea605fe8e4c497f0c
threehundo <samuelbaik87@gmail.com>
2021-03-22 11:29:00 -0700

    Styled cards to bump up font and leave pertinent info only in ProductsAll component.

--- src/components/ProductsAll.js
+++ src/components/ProductsAll.js
@@ -23,32 +23,26 @@ const ProductsAll = ({ handleClick, data, error, isPending }) => {
           <div
             key={snkr.id}
             id={snkr.id}
-            className="flex flex-col self-center justify-evenly rounded-lg border-2 border-white w-11/12 h-auto py-2 px-2 my-2 mx-auto"
+            className="flex flex-col self-center justify-evenly rounded-lg border-2 border-white w-11/12 h-auto py-2 px-2 my-2 mx-auto mb-5"
           >
-            <Link to={`/catalogue/${snkr.linkID}`}>
-              <img
-                className="rounded-t-xl"
-                alt="snkrImg"
-                src={snkr.img.smallImg}
-              ></img>
-              <h2 className="text-lg">{snkr.shoe}</h2>
-              <h2 className="text-base">"{snkr.name}"</h2>
-            </Link>
-            <p className="text-xs">Released: {snkr.releaseDate}</p>
-            <p className="text-xs">
-              Since his game-winning shot that brought championship glory to
-              North Carolina, Michael Jordan has been at the forefront of
-              basketball consciousness. He took the court in 1985 wearing the
-              original Air Jordan I, simultaneously breaking league rules and
-              his opponents' will while capturing the imagination of fans
-              worldwide.
-            </p>
-            <button
-              className="bg-secondary text-primary rounded-lg ring-4 ring-primary my-2 rounded-full py-1 px-2 hover:text-secondary hover:bg-primary hover:ring-secondary focus:outline-none focus:bg-primary focus:ring-secondary focus:text-secondary"
-              onClick={addToCart}
-            >
-              Buy
-            </button>
+            <div className="text-center">
+              <Link to={`/catalogue/${snkr.linkID}`}>
+                <img
+                  className="rounded-xl"
+                  alt="snkrImg"
+                  src={snkr.img.smallImg}
+                ></img>
+              </Link>
+              <h2 className="text-3xl">{snkr.shoe}</h2>
+              <h2 className="text-5xl">"{snkr.name}"</h2>
+              <h2 className="text-3xl">${snkr.retailPrice}</h2>
+              <button
+                className="bg-secondary text-primary rounded-lg ring-4 ring-primary my-2 rounded-full py-1 px-2 hover:text-secondary hover:bg-primary hover:ring-secondary focus:outline-none focus:bg-primary focus:ring-secondary focus:text-secondary"
+                onClick={addToCart}
+              >
+                Add to Cart
+              </button>
+            </div>
           </div>
         );
       });

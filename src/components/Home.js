b964123eea70da26067d0e4d2b07370880249287
threehundo <samuelbaik87@gmail.com>
2021-03-22 11:01:46 -0700

    Reformatted homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,24 +13,22 @@ const Home = () => {
         : "translateY(300%)",
   });
   return (
-<<<<<<< HEAD
     <div className="flex flex-column w-auto h-full">
-=======
-    <div className="flex flex-column w-auto h-full overflow-y-hidden">
->>>>>>> 1eef792a39c39fcc292dcad80686381c61173106
-      <div className="flex-1 bg-hero-pattern bg-contain bg-fixed bg-no-repeat bg-center w-screen">
-        <animated.div
-          id="springUp"
-          style={{ ...springUp, overflow: "hidden" }}
-          className="z-0 w-80 absolute inset-x-0 bottom-28 bg-primary my-5 mx-auto p-2 shadow-2xl border-4 border-white rounded opacity-0	"
-        >
-          <h2 className="text-center text-3xl">Air Jordan 1</h2>
-          <p className="text-sm text-justify">
-            The Jordans that started it all. The Air Jordan 1 is the most
-            important model in sneaker history. Find every essential Air Jordan
-            1 High colorway here. We take it back to Rarified Air.
-          </p>
-        </animated.div>
+      <div className="flex flex-column w-auto h-full overflow-y-hidden">
+        <div className="flex-1 bg-hero-pattern bg-contain bg-fixed bg-no-repeat bg-center w-screen">
+          <animated.div
+            id="springUp"
+            style={{ ...springUp, overflow: "hidden" }}
+            className="z-0 w-80 absolute inset-x-0 bottom-28 bg-primary my-5 mx-auto p-2 shadow-2xl border-4 border-white rounded opacity-0	"
+          >
+            <h2 className="text-center text-3xl">Air Jordan 1</h2>
+            <p className="text-sm text-justify">
+              The Jordans that started it all. The Air Jordan 1 is the most
+              important model in sneaker history. Find every essential Air
+              Jordan 1 High colorway here. We take it back to Rarified Air.
+            </p>
+          </animated.div>
+        </div>
       </div>
     </div>
   );

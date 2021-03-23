c1b55bce1dcdc3e2459d6aef97d61f5f8c668e7d
threehundo <samuelbaik87@gmail.com>
2021-03-23 11:08:39 -0700

    Cleaned up CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { useSpring, animated } from "react-spring";
 const Home = () => {
   const springUp = useSpring({
     config: {
-      duration: 250,
+      duration: 200,
     },
     opacity: document.readyState === "complete" ? 1 : 0,
     transform:
@@ -14,13 +14,13 @@ const Home = () => {
   });
 
   return (
-    <div className="flex flex-column w-auto h-full">
+    <div className="w-auto h-full">
       <div className="flex flex-column w-auto h-full overflow-y-hidden">
-        <div className="flex-1 bg-hero-pattern bg-contain bg-fixed bg-no-repeat bg-center w-screen">
+        <div className="bg-hero-pattern bg-contain bg-fixed bg-no-repeat bg-center w-screen">
           <animated.div
             id="springUp"
             style={{ ...springUp, overflow: "hidden" }}
-            className="z-0 w-80 absolute inset-x-0 bottom-28 bg-primary my-5 mx-auto p-2 shadow-2xl border-4 border-white rounded opacity-0	"
+            className="z-0 w-80 absolute inset-x-0 bottom-28 my-5 mx-auto p-2 shadow-2xl border-4 border-white rounded opacity-50"
           >
             <h2 className="text-center text-3xl">Air Jordan 1</h2>
             <p className="text-sm text-justify">
